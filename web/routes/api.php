eacaf31432b2d442d7b13a1abe14437872b988ba
Filipe <filipe.cavalcante.slice@estadao.com>
2021-10-18 20:50:00 -0300

    jwt working

---
+++ web/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+
+Route::post('login', [ApiController::class, 'authenticate']);
+Route::post('register', [ApiController::class, 'register']);
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::get('logout', [ApiController::class, 'logout']);
+    Route::get('get-user', [ApiController::class, 'get_user']);
+});
